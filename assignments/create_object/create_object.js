fc2d1bb1e312a9442335dd9a1afe3f5a3b200ef0
Brahmam <ceasar1729@gmail.com>
2022-01-23 17:13:55 +0530

    CreatObj

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,10 @@
 function CreateObject(arr) {
     // Write your code here
+    var d ={};
+    for (let i =1; i<arr.length;i+=2){
+        d[arr[i-1]] = arr[i];
+    }
+    return d;
 }
 
 module.exports = CreateObject;
